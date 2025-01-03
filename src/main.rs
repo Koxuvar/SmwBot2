0c28282d1506bba576b8ffd93d2b252cd2d902d8
Koxuvar <connor@mmvdesigns.com>
2025-01-02 20:42:45 -0600

    feat: hello github

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use dotenv::dotenv;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+use tracing::info;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        info!("received message from: {}", msg.author);
+        if msg.content == "!hey" {
+            let resp  = format!("Hello {}", msg.author);
+            if let Err(why) = msg.channel_id.say(&ctx.http, resp).await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    dotenv().ok();
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    // Create a new instance of the Client, logging in as a bot.
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    info!("starting server");
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
