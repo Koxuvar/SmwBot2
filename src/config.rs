e7dce54561845ca97d615e320f11579e6f899d05
StillNotAPAC-WSL <connor@somidwest.com>
2025-01-10 15:32:48 -0600

    goofed on some thigns

--- src/config.rs
+++ src/config.rs
@@ -3,14 +3,12 @@ use serde::Deserialize;
 #[derive(Deserialize)]
 pub struct ConfigurationData {
     pub bot: BotConfig,
-    pub api: ApiConfig
 }
 
 #[derive(Deserialize)]
 pub struct BotConfig {
     pub general: GeneralConfig,
     pub discord: DiscordConfig,
-    pub denylist: DenylistConfig,
     pub logging: LoggingConfig
 }
 
@@ -31,47 +29,3 @@ pub struct DiscordConfig {
     pub token: String
 }
 
-#[derive(Deserialize)]
-pub struct DenylistConfig {
-    pub spotify: DenylistSpotifyConfig
-}
-
-#[derive(Deserialize)]
-pub struct DenylistSpotifyConfig {
-    pub ids: Vec<u64>
-}
-
-#[derive(Deserialize)]
-pub struct ApiConfig {
-    pub entertainment: EntertainmentConfig,
-    pub music: MusicConfig,
-    pub services: ServicesConfig
-}
-
-#[derive(Deserialize)]
-pub struct EntertainmentConfig {
-    pub tmdb: String
-}
-
-#[derive(Deserialize)]
-pub struct MusicConfig {
-    pub spotify: SpotifyConfig,
-    pub lastfm: LastFmConfig
-}
-
-#[derive(Deserialize)]
-pub struct ServicesConfig {
-    pub github: String,
-    pub google: String
-}
-
-#[derive(Deserialize)]
-pub struct SpotifyConfig {
-    pub client_id: String,
-    pub client_secret: String
-}
-
-#[derive(Deserialize)]
-pub struct LastFmConfig {
-    pub api_key: String
-}
